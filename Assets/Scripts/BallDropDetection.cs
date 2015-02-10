1464396be5554145a5fdc16b9e61a757ce125cb8
Clay <c.ewing@miami.edu>
2015-02-09 22:15:37 -0500

    Snow correction

---
+++ Assets/Scripts/BallDropDetection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallDropDetection : MonoBehaviour {
+	public TerrainCollider terrain;
+	public Camera holeCam;
+	public GameObject ball;
+	public GameObject pole;
+	private bool ballDropped = false;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		holeCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ballDropped) {
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		ballDropped = true;
+		terrain.collider.enabled = false;
+		holeCam.enabled = true;
+		ball.rigidbody.velocity = Vector3.zero;
+		ball.transform.LookAt(pole.transform);
+		ball.rigidbody.AddRelativeForce(Vector3.forward, ForceMode.Force);
+		ball.rigidbody.AddForce(0,-10f,0);
+
+	}
+
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public Transform target;
+	public float smooth = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 modifiedTarget = new Vector3(target.position.x, target.position.y, target.position.z);
+		transform.position = Vector3.Lerp(transform.position, modifiedTarget, Time.deltaTime * smooth);
+	}
+}
